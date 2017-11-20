1a343ef1fb1d7655f87928c7333de7cde5f6da6a
Ryan Andersen <ryan.andersen05@gmail.com>
2017-11-20 13:59:12 -0500

    More Debug updates added to device manager

--- DragonDeviceManager.py
+++ DragonDeviceManager.py
@@ -3,24 +3,6 @@ import DragonMasterSerialDevice
 
 
 
-###################Debug String Methods########################
-"""
-Returns a new string that is of the desired length
-"""
-def set_string_length(string1, lengthOfString, spacingChar = ' '):
-    remainingLength = lengthOfString - len(string1)
-    newStringToReturn = ''
-    if remainingLength > 0:
-        newStringToReturn = [spacingChar] * int(remainingLength)
-    newStringToReturn = newStringToReturn + string1
-    remainingLength = lengthOfString - len(newStringToReturn)
-    if remainingLength > 0:
-        newStringToReturn += [spacingChar] * remainingLength
-
-    return newStringToReturn
-
-
-###############################################################
 
 
 class DragonMasterDeviceManager:
@@ -70,8 +52,6 @@ class DragonMasterDeviceManager:
         if not self.deviceDictionary.has_key(playerStationParentDeviceKey):
             return
         playerStation = self.deviceDictionary[playerStationParentDeviceKey]
-
-        print
         pass
 
     ########################################################################
@@ -93,7 +73,6 @@ class DragonMasterDevice:
 
 """
 PlayerStation contains a reference to each device that should be attached to the player station in out cabinet
-
 """
 class PlayerStation:
 
@@ -105,3 +84,37 @@ class PlayerStation:
         self.parentDevicePathKey = parentDevicePathKey
         return
 
+
+
+###################Debug String Methods########################
+"""
+Returns a new string that is of the desired length. Fills in remaining space with
+spacingChar value. Make sure that spacingChar is of length 1 if you want accurately spaced
+string
+"""
+def set_string_length(string1, lengthOfString = 60, spacingChar = ' '):
+    remainingLength = lengthOfString - len(string1)
+    newStringToReturn = ''
+    if remainingLength > 0:
+        newStringToReturn = [spacingChar] * int(remainingLength)
+    newStringToReturn = newStringToReturn + string1
+    remainingLength = lengthOfString - len(newStringToReturn)
+    if remainingLength > 0:
+        newStringToReturn += [spacingChar] * remainingLength
+
+    return newStringToReturn
+
+"""
+Returns a new string that is of size lenghtOfString. Fills the remaining space between string1 and string2
+with the char spacingChar. Please make sure that the variable spacingChar is of length = 1 if you want
+accurately sized string.
+"""
+def set_string_length_multiple(string1, string2, lengthOfString = 60, spacingChar = ' '):
+    remainingLength = lengthOfString - len(string1) - len(string2)
+    newStringToReturn = ''
+    if remainingLength > 0:
+        return string1 + ([spacingChar] * remainingLength) + string2
+    else:
+        return stirng1 + string2
+
+###############################################################
