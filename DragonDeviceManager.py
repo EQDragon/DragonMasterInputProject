b5b3fe198670ed740ef92e686a72b437f4ff5b8a
Ryan Andersen <ryan.andersen05@gmail.com>
2017-11-20 13:35:34 -0500

    Updates to Debug Methods for Device Manager

--- DragonDeviceManager.py
+++ DragonDeviceManager.py
@@ -1,17 +1,29 @@
 import threading
+import DragonMasterSerialDevice
 
 
 
+###################Debug String Methods########################
+"""
+Returns a new string that is of the desired length
+"""
+def set_string_length(string1, lengthOfString, spacingChar = ' '):
+    remainingLength = lengthOfString - len(string1)
+    newStringToReturn = ''
+    if remainingLength > 0:
+        newStringToReturn = [spacingChar] * int(remainingLength)
+    newStringToReturn = newStringToReturn + string1
+    remainingLength = lengthOfString - len(newStringToReturn)
+    if remainingLength > 0:
+        newStringToReturn += [spacingChar] * remainingLength
 
+    return newStringToReturn
+
+
+###############################################################
 
 
 class DragonMasterDeviceManager:
-    ###########################################################
-    DRAX_DICTIONARY_INDEX = 0
-    DBV_DICTIONARY_INDEX = 1
-    JOYSTICK_DICTIONARY_INDEX = 2
-    PRINTER_DICTIONARY_INDEX = 3
-    ###########################################################
 
     def __init__(self):
         self.deviceList = []
@@ -25,7 +37,44 @@ class DragonMasterDeviceManager:
 
     def remove_device(self, dragonMasterDevice):
         if not self.deviceDictionary.has_key(dragonMasterDevice.devicePath):
+
             pass
+
+
+    #############Contains Methods############################################
+    def manager_contains_drax_device(self, draxDevice):
+        if draxDevice is not DragonMasterSerialDevice.Draxboard:
+            return
+
+
+        for playerStation in self.deviceDictionary.items():
+            if playerStation != None and playerStation.draxboardDevice != None:
+
+                pass
+
+            pass
+
+        return
+
+    def manager_contains_dbv_device(self, dbvDevice):
+
+        return
+
+    def manager_contains_joystick_device(self, joystickDevice):
+
+        return
+    ########################################################################
+    ####################Debug Methods#######################################
+
+    def print_player_station_status(self, playerStationParentDeviceKey):
+        if not self.deviceDictionary.has_key(playerStationParentDeviceKey):
+            return
+        playerStation = self.deviceDictionary[playerStationParentDeviceKey]
+
+        print
+        pass
+
+    ########################################################################
 
 
 """
@@ -48,9 +97,11 @@ PlayerStation contains a reference to each device that should be attached to the
 """
 class PlayerStation:
 
-    def __init__(self, draxboardDevice=None, dbvDevice=None, joystickDevice=None, printerDevice=None):
+    def __init__(self, draxboardDevice=None, dbvDevice=None, joystickDevice=None, printerDevice=None, parentDevicePathKey = None):
         self.draxboardDevice = draxboardDevice
         self.dbvDevice = dbvDevice
         self.joystickDevice = joystickDevice
         self.printerDevice = printerDevice
+        self.parentDevicePathKey = parentDevicePathKey
+        return
 
